79a881699e1e75bd8c908a873395202f0978f4b6
Gene Boggs <gene@ology.net>
2025-09-03 17:56:05 -0700

    More

--- tests.py
+++ tests.py
@@ -8,9 +8,47 @@ motif = r.motif()
 assert isinstance(motif, list), "motif is not a list"
 
 r = Rhythm(
-    measure_size=1,
-    durations=[1],
+    durations=[4],
 )
 assert r.weights == [1], "weights not correct"
 motif = r.motif()
-assert motif == [1], "motif not correct"
+assert motif == [4], "motif not correct"
+
+r = Rhythm(
+    durations=[1],
+)
+motif = r.motif()
+assert motif == [1,1,1,1], "motif not correct"
+
+r = Rhythm(
+    durations=[2/3],
+)
+motif = r.motif()
+assert motif == [2/3 for x in range(6)], "motif not correct"
+
+r = Rhythm(
+    measure_size=8,
+    durations=[4],
+)
+motif = r.motif()
+assert motif == [4,4], "motif not correct"
+
+r = Rhythm(
+    measure_size=100,
+    durations=[50],
+)
+motif = r.motif()
+assert motif == [50,50], "motif not correct"
+
+r = Rhythm(
+    durations=[1,2],
+    weights=[0,1],
+)
+motif = r.motif()
+assert motif == [2,2], "motif not correct"
+
+r = Rhythm(
+    durations=[3],
+)
+motif = r.motif()
+assert motif == [3,1], "motif not correct"
